6069585ec6b26274cba20d32da6eec94498ce07e
juanjo <jlopezco@unsa.edu.pe>
2023-03-21 21:10:14 -0500

    Cadenas

--- RetoMicrosoft/python/listas.py
+++ RetoMicrosoft/python/listas.py
@@ -6,3 +6,39 @@ print(ultimo)
 print("The last planet is", planets[-1])
 print("The penultimate planet is", planets[-2]) 
 planets.append('Pluto')
+
+
+
+gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
+bus_weight = 12650 # in kilograms, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "kg")
+print("On Mercury, a double-decker bus weighs", bus_weight * gravity_on_planets[0], "kg")
+
+bus_weight = 12650 # in kilograms, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "kg")
+print("The lightest a bus would be in the solar system is", bus_weight * min(gravity_on_planets), "kg")
+print("The heaviest a bus would be in the solar system is", bus_weight * max(gravity_on_planets), "kg")
+
+###########
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+planets_before_earth = planets[0:2]
+print(planets_before_earth)
+planets_after_earth = planets[1:]
+print(planets_after_earth)
+
+###########
+
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+#################
+regular_satellite_moons = amalthea_group + galilean_moons
+regular_satellite_moons.sort()
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+################
+
+regular_satellite_moons.sort(reverse=True)
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
