1acdd0579a8867324d788837527334eceafdef21
juanjo <jlopezco@unsa.edu.pe>
2023-03-22 21:57:44 -0500

    Diccionarios

--- RetoMicrosoft/python/diccionarios.py
+++ RetoMicrosoft/python/diccionarios.py
@@ -51,3 +51,44 @@ planet['diameter (km)'] = {
 # }
 
 print(f'{planet["name"]} polar diameter: {planet["diameter (km)"]["polar"]}')
+
+
+planet = {
+    'name': 'Mars',
+    'moons': 2
+}
+
+
+print(planet['name'])
+print(f"{planet['name']} has {planet['moons']}")
+planet['circumference']={
+    'polar': 6752,
+    'equatorial': 6792
+}
+print(f"{planet['name']} has {planet['circumference']['polar']}")
+
+#.keys() and .values()
+planet_moons={
+'mercury': 0,
+'venus': 0,
+'earth': 1,
+'mars': 2,
+'jupiter': 79,
+'saturn': 82,
+'uranus': 27,
+'neptune': 14,
+'pluto': 5,
+'haumea': 2,
+'makemake': 1,
+'eris': 1    
+}
+
+moons = list(planet_moons.values())
+total_planets = len(planet_moons.keys())
+
+print(moons)
+total_moons=0
+for moon in moons:
+    total_moons+=moon
+print(total_moons)
+print(total_moons/total_planets)
