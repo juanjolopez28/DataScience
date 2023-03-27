82f5c8338eeb2a5279062379d67b5fa0ad6d8bd0
juanjo <jlopezco@unsa.edu.pe>
2023-03-26 22:22:26 -0500

    Subconsultas

---
+++ RetoMicrosoft/sql/funciones_integradas.sql
@@ -0,0 +1,89 @@
+
+/*FUNCIONES ESCALRES*/
+
+/*En el ejemplo hipotético siguiente se usan varias funciones de fecha y hora:*/
+SELECT  SalesOrderID,
+    OrderDate,
+        YEAR(OrderDate) AS OrderYear,
+        DATENAME(mm, OrderDate) AS OrderMonth,
+        DAY(OrderDate) AS OrderDay,
+        DATENAME(dw, OrderDate) AS OrderWeekDay,
+        DATEDIFF(yy,OrderDate, GETDATE()) AS YearsSinceOrder
+FROM Sales.SalesOrderHeader;
+
+/*En el ejemplo siguiente se incluyen algunas funciones matemáticas:*/
+SELECT TaxAmt,
+       ROUND(TaxAmt, 0) AS Rounded,
+       FLOOR(TaxAmt) AS Floor,
+       CEILING(TaxAmt) AS Ceiling,
+       SQUARE(TaxAmt) AS Squared,
+       SQRT(TaxAmt) AS Root,
+       LOG(TaxAmt) AS Log,
+       TaxAmt * RAND() AS Randomized
+FROM Sales.SalesOrderHeader;
+
+
+/*En el ejemplo siguiente se usan algunas funciones de cadena:*/
+SELECT  CompanyName,
+        UPPER(CompanyName) AS UpperCase,
+        LOWER(CompanyName) AS LowerCase,
+        LEN(CompanyName) AS Length,
+        REVERSE(CompanyName) AS Reversed,
+        CHARINDEX(' ', CompanyName) AS FirstSpace,
+        LEFT(CompanyName, CHARINDEX(' ', CompanyName)) AS FirstWord,
+        SUBSTRING(CompanyName, CHARINDEX(' ', CompanyName) + 1, LEN(CompanyName)) AS RestOfName
+FROM Sales.Customer;
+
+/*IIF*/
+
+SELECT AddressType,
+      IIF(AddressType = 'Main Office', 'Billing', 'Mailing') AS UseAddressFor
+FROM Sales.CustomerAddress;
+
+/*CHOOSE*/
+
+SELECT SalesOrderID, Status,
+CHOOSE(Status, 'Ordered', 'Shipped', 'Delivered') AS OrderStatus
+FROM Sales.SalesOrderHeader;
+
+
+
+/*FUNCIONES DE CLASIFICACION Y CONJUNTO DE FILAS*/
+
+SELECT TOP 100 ProductID, Name, ListPrice,
+RANK() OVER(ORDER BY ListPrice DESC) AS RankByPrice
+FROM Production.Product AS p
+ORDER BY RankByPrice;
+
+
+/*Para usar servidores remotos, debe habilitar algunas opciones avanzadas en la instancia de SQL Server en la que se ejecuta la consulta.*/
+
+SELECT a.*
+FROM OPENROWSET('SQLNCLI', 'Server=SalesDB;Trusted_Connection=yes;',
+    'SELECT Name, ListPrice
+    FROM AdventureWorks.Production.Product') AS a;
+
+
+
+/*FUNCIONES DE AGREGADO*/
+SELECT AVG(ListPrice) AS AveragePrice,
+       MIN(ListPrice) AS MinimumPrice,
+       MAX(ListPrice) AS MaximumPrice
+FROM Production.Product;
+
+
+SELECT COUNT(DISTINCT CustomerID) AS UniqueCustomers
+FROM Sales.SalesOrderHeader;
+
+
+SELECT SUM(c2) AS sum_nonnulls, 
+    COUNT(*) AS count_all_rows, 
+    COUNT(c2) AS count_nonnulls, 
+    AVG(c2) AS average, 
+    (SUM(c2)/COUNT(*)) AS arith_average
+FROM t1;
+
+
+
+
+
