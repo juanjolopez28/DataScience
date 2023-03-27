328c0dcae1c99ae0f5c9fcfb779f10937fcfbd1e
juanjo <jlopezco@unsa.edu.pe>
2023-03-26 20:17:43 -0500

    Subconsultas

--- RetoMicrosoft/sql/subconsultas.sql
+++ RetoMicrosoft/sql/subconsultas.sql
@@ -39,3 +39,63 @@ WHERE SalesOrderID =
      FROM SalesLT.SalesOrderHeader AS o2
      WHERE o2.CustomerID = o1.CustomerID)
 ORDER BY CustomerID, OrderDate;
+
+
+SELECT CustomerID, CompanyName, EmailAddress 
+FROM Sales.Customer AS c 
+WHERE EXISTS
+(SELECT * 
+  FROM Sales.SalesOrderHeader AS o
+  WHERE o.CustomerID = c.CustomerID);
+
+
+SELECT CustomerID, CompanyName, EmailAddress 
+FROM SalesLT.Customer AS c 
+WHERE NOT EXISTS
+  (SELECT * 
+   FROM SalesLT.SalesOrderHeader AS o
+   WHERE o.CustomerID = c.CustomerID); 
+
+
+/* LABORATORIO*/
+SELECT MAX(UnitPrice)FROM SalesLT.SalesOrderDetail;
+SELECT Name, ListPriceFROM SalesLT.Product
+WHERE ListPrice >    
+(SELECT MAX(UnitPrice)     FROM SalesLT.SalesOrderDetail);
+
+SELECT DISTINCT ProductIDFROM SalesLT.SalesOrderDetail
+WHERE OrderQty >= 20;
+
+SELECT Name FROM SalesLT.Product
+WHERE ProductID IN    
+(SELECT DISTINCT ProductID     
+FROM SalesLT.SalesOrderDetail     
+WHERE OrderQty >= 20);
+
+/*ESTO HACE LO MISMO*/
+SELECT DISTINCT Name
+FROM SalesLT.Product AS p
+JOIN SalesLT.SalesOrderDetail AS o
+    ON p.ProductID = o.ProductID
+WHERE OrderQty >= 20;
+
+/*CORRELACIONADAS*/
+
+SELECT od.SalesOrderID, od.ProductID, od.OrderQtyFROM SalesLT.SalesOrderDetail AS od
+WHERE od.OrderQty =    
+(SELECT MAX(OrderQty)     FROM SalesLT.SalesOrderDetail AS d     
+WHERE od.ProductID = d.ProductID)ORDER BY od.ProductID;
+
+/*LABORATORIO*/
+SELECT ProductID,Name,ListPrice FROM SalesLT.Product
+WHERE ListPrice> 
+(SELECT AVG(UnitPrice) FROM SalesLT.SalesOrderDetail)
+
+/*CORRELACIONADAS*/
+SELECT p.ProductID, p.Name, p.StandardCost, p.ListPrice,
+    (SELECT AVG(o.UnitPrice)
+     FROM SalesLT.SalesOrderDetail AS o
+     WHERE p.ProductID = o.ProductID) AS AvgSellingPrice
+FROM SalesLT.Product AS p
+ORDER BY p.ProductID;
+
